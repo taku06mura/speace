a0d86851aa2b08b17fa0f34a2e34844f34f224aa
taku06mura <t.lux.g1106@icloud.com>
2020-02-29 21:48:49 +0900

    新規投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   resources :users, only: [:edit, :update]
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
 end
