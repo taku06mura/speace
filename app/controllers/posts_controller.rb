ca080b819efd145954399f9e376c7a91c721a766
taku06mura <t.lux.g1106@icloud.com>
2020-03-11 17:39:10 +0900

    pagination 機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # before_action :set_post, only:[:show, :edit]
 
   def index
-    @posts = Post.all
+    @posts = Post.page(params[:page])
   end
 
   def new
