da3bac840c6ce60fac0ecd3729e4a7dc122de4db
taku06mura <t.lux.g1106@icloud.com>
2020-03-12 17:36:44 +0900

    profile new page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,6 @@
 class ProfilesController < ApplicationController
+
+  def new
+    @profiles = Profile.new
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :posts
+  has_one :profile, dependent: :destroy
 end
