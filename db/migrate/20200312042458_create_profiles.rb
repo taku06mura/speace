9d36f7081c57f4a13f817e4146093030c69bdd55
taku06mura <t.lux.g1106@icloud.com>
2020-03-12 14:21:06 +0900

    profile.model migrate

---
+++ db/migrate/20200312042458_create_profiles.rb
@@ -0,0 +1,12 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :nickname,      null: false
+      t.string :sex,           null: false
+      t.string :introduction,    null: false
+      t.string :image
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_062211) do
+ActiveRecord::Schema.define(version: 2020_03_12_042458) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 2020_02_26_062211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "sex", null: false
+    t.string "introduction", null: false
+    t.string "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -37,4 +48,5 @@ ActiveRecord::Schema.define(version: 2020_02_26_062211) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
 end
