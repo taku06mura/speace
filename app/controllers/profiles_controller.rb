a1eecf34929b0d382eb23c0a56092439406ff16e
taku06mura <t.lux.g1106@icloud.com>
2020-03-13 15:29:45 +0900

    Update profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,15 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
   end
+
+  def create
+    profile = Profile.create(profile_params)
+    
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:nickname, :introduction, :image).merge(user_id: current_user.id)
+  end
 end
