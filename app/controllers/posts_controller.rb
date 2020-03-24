e4ed3f2c888658b6a1f0abb484b75388ed1927f3
taku06mura <t.lux.g1106@icloud.com>
2020-03-24 18:07:31 +0900

    post-edit view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+
   def destroy
     post = Post.find(params[:id])
     post.destroy
