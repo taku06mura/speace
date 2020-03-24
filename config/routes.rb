41341655979eb2b800b341ec75b92c4f7008b18b
taku06mura <t.lux.g1106@icloud.com>
2020-03-24 17:54:03 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   resources :users, only: [:edit, :update]
-  resources :posts, only: [:index, :new, :create, :show, :destroy]
+  resources :posts
   resources :profiles, only:[:new, :create, :show, :edit, :update]
 end
