b14b79bc016308082123e318db5a3dae16a8207c
taku06mura <t.lux.g1106@icloud.com>
2020-03-26 23:47:46 +0900

    new action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,22 @@
 class TeamsController < ApplicationController
 
   def new
+    @team = Team.new
+    @team.users << current_user
   end
 
   def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to root_path, notice: 'チームを作成しました'
+    else
+      render :new
+    end
   end
+
+  private
+  def team_prarams
+    params.require(team).permit(:mame, user_ids: [])
+  end
+
 end
