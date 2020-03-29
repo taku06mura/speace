bf7a13adf3faab897985a686887c08fd2e2716db
taku06mura <t.lux.g1106@icloud.com>
2020-03-29 11:02:10 +0900

    update team view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -29,7 +29,7 @@ class TeamsController < ApplicationController
 
   private
   def team_params
-    params.require(:team).permit(:name, user_ids: [])
+    params.require(:team).permit(:name, user_ids: [] )
   end
 
 end
