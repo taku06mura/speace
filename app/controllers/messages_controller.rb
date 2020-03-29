99b5ef75df54c4d5da9ae7376de51c3b13ed0c65
taku06mura <t.lux.g1106@icloud.com>
2020-03-29 18:31:49 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,30 @@
 class MessagesController < ApplicationController
+  before_action :set_team
 
   def index
-
+    @message = Message.new
+    @messages = @team.messages.includes(:user)
   end
 
   def create
-    
+    @message = @team.messages.new(message_params)
+    if @message.save
+      redirect_to team_messages_path(@team), notice: 'メッセージを送信しました'
+    else
+      @messages = @team.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力して下さい'
+      render :index
+    end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image)merge.(user_id: current_user.id)
+  end
+
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
+
 end
