9154b936ea8f3f2ea36148fe4066caa071ddd1e9
taku06mura <t.lux.g1106@icloud.com>
2020-03-31 00:43:21 +0900

    message index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :posts
   resources :profiles, only: [:new, :create, :show, :edit, :update]
-  resources :teams, only: [:index, :new, :create, :edit, :update]
+  resources :teams, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
 end
