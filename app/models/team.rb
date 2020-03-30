eca61be847accfe7c16b9d4f72b255ea7298e1c7
taku06mura <t.lux.g1106@icloud.com>
2020-03-30 19:02:22 +0900

    show last message

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,19 @@ class Team < ApplicationRecord
   has_many :team_users
   has_many :users, through: :team_users
   has_many :messages
+
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
+  
 end
